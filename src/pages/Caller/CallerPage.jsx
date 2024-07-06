e2130d3d086d075830b143568593f5b030b01de4
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-07-06 07:21:05 +0300

    Update CallerPage.jsx

--- src/pages/Caller/CallerPage.jsx
+++ src/pages/Caller/CallerPage.jsx
@@ -39,7 +39,7 @@ export default function CallerPage() {
 
   useEffect(() => {
     dispatch(getCallerCurrentWeekByCourse(courseId));
-  }, [courseId, dispatch]);
+  }, []);
 
   useEffect(() => {
     if (weekId) {
