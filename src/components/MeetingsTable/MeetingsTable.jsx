8786170ca41ecc600e87ba99e3ee35ba31b38583
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-10-02 17:16:42 +0300

    Update MeetingsTable.jsx
    fix postpone bug

--- src/components/MeetingsTable/MeetingsTable.jsx
+++ src/components/MeetingsTable/MeetingsTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment, useState } from "react";
+import React, { useEffect, Fragment, useState, useCallback } from "react";
 import styles from "./MeetingsTable.module.scss";
 import MeetingsTableItem from "../MeetingsTableItem/MeetingsTableItem";
 import { useSelector, useDispatch } from "react-redux";
@@ -12,10 +12,9 @@ import { getTypeSelection } from "../../redux/manager/manager-selectors";
 import {
   changeStatusSlot,
   setManagerError,
-  setManagerLoading
+  setManagerLoading,
 } from "../../redux/manager/manager-operations";
 import { updateSlot } from "../../helpers/week/week";
-
 
 const MeetingsTable = ({
   isLoading,
@@ -33,12 +32,12 @@ const MeetingsTable = ({
   setSelectedManagerIds,
   date,
   getNewTableData,
-  hideFreeSlots
+  hideFreeSlots,
 }) => {
   const indefyTimedSlotText = (timeid) => {
     switch (timeid) {
       case 1:
-        return "Avaliable";
+        return "Available";
       case 2:
         return "Working time";
       case 6:
@@ -58,13 +57,45 @@ const MeetingsTable = ({
 
   let isTableLengthD = false;
   let isMarkUpAdded = false;
-  const managerLoading = useSelector(isManagerLoading);
-  const callerLoading = useSelector(getCallerLoading);
-  
-  // table = table.sort((a, b) => a.manager_name.localeCompare(b.manager_name));
-  
+  const [managerLoading, setManagerLoading] = useState(false);
+  const [callerLoading, setCallerLoading] = useState(false);
+
   const dispatch = useDispatch();
   const typeSelection = useSelector(getTypeSelection);
+
+  // Виклик useCallback поза умовами
+  const Fn = useCallback(
+    (id, week, day, time) => {
+      return new Promise((resolve, reject) => {
+       setManagerLoading(true);
+        const statusMap = {
+          Consultations: 1,
+          "Working time": 2,
+          Free: 0,
+        };
+
+        const status = statusMap[typeSelection] ?? 0;
+
+        updateSlot(id, week, day, time, status)
+          .then(() => {
+            dispatch(
+              changeStatusSlot({
+                day,
+                time,
+                colorId: status,
+              })
+            );
+            resolve();
+          })
+          .catch((error) => {
+            dispatch(setManagerError(error.message));
+            reject(error);
+          })
+          .finally(() => setManagerLoading(false));
+      });
+    },
+    [dispatch, typeSelection]
+  );
 
   if (isStatusSorted) {
     table = table.filter((item) => {
@@ -74,214 +105,168 @@ const MeetingsTable = ({
       );
     });
   }
+
   if (currentSelectedStars) {
     table = table.filter((item) => {
-      return (
-        item.manager_appointments[tableTime - 8]?.follow_up === true
+      return item.manager_appointments[tableTime - 8]?.follow_up === true;
+    });
+  }
+
+  if (currentSelectedStarsTable) {
+    table = table.filter((item) => {
+      return item.manager_appointments.some(
+        (appointment) => appointment.follow_up === true
       );
     });
   }
-  if (currentSelectedStarsTable) {
-    table = table.filter((item) => {
-        // Перевіряємо, чи є хоча б один об'єкт у manager_appointments з follow_up === true
-        return item.manager_appointments.some(appointment => appointment.follow_up === true);
-    });
-}
+
   if (hideFreeSlots) {
     table = table.filter((item) => {
-        return item.manager_appointments.some(appointment => appointment.status > 0);
+      return item.manager_appointments.some(
+        (appointment) => appointment.status > 0
+      );
     });
-}
-  if (table.length === 0) {
-    isTableLengthD = true;
-    return (
-      <MeetingsTableItem key={uuidv4()} text={"no data has been founded"} />
-    );
   }
 
-  function Fn(id, week, day, time) {
-    return new Promise((resolve, reject) => {
-      switch (typeSelection) {
-        case "Consultations":
-          dispatch(setManagerLoading(true));
-          updateSlot(id, week, day, time, 1)
-            .then(() => {
-              dispatch(
-                changeStatusSlot({
-                  day,
-                  time,
-                  colorId: 1,
-                })
-              );
-              resolve(); // Розділено виклик resolve після успішного виконання
-            })
-            .catch((error) => {
-              dispatch(setManagerError(error.message));
-              reject(error); // Розділено виклик reject у випадку помилки
-            })
-            .finally(() => dispatch(setManagerLoading(false)));
-          break;
-        case "Working time":
-          dispatch(setManagerLoading(true));
-          updateSlot(id, week, day, time, 2)
-            .then(() => {
-              dispatch(
-                changeStatusSlot({
-                  day,
-                  time,
-                  colorId: 2,
-                })
-              );
-              resolve(); // Розділено виклик resolve після успішного виконання
-            })
-            .catch((error) => {
-              dispatch(setManagerError(error.message));
-              reject(error); // Розділено виклик reject у випадку помилки
-            })
-            .finally(() => dispatch(setManagerLoading(false)));
-          break;
-        case "Free":
-          dispatch(setManagerLoading(true));
-          updateSlot(id, week, day, time, 0)
-            .then(() => {
-              dispatch(
-                changeStatusSlot({
-                  day,
-                  time,
-                  colorId: 0,
-                })
-              );
-              resolve(); // Розділено виклик resolve після успішного виконання
-            })
-            .catch((error) => {
-              dispatch(setManagerError(error.message));
-              reject(error); // Розділено виклик reject у випадку помилки
-            })
-            .finally(() => dispatch(setManagerLoading(false)));
-          break;
-        default:
-          break;
-      }
-    });
+  if (table.length === 0) {
+    isTableLengthD = true;
+    return <MeetingsTableItem key={uuidv4()} text={"No data found"} />;
   }
 
   return (
     <div key={uuidv4()} className={styles.wrapperTable}>
       {(managerLoading || callerLoading) && (
         <div className={styles.spinner}>
-        <TailSpin height="130px" width="130px" color="#999DFF" />
+          <TailSpin height="130px" width="130px" color="#999DFF" />
         </div>
       )}
       <div>
-      {isLoading ? <div className={styles.loadingBackdrop}><TailSpin height="150px" width="150px" color="#999DFF" /></div> : null}
-      <ul
-        key={uuidv4()}
-        className={!isListView ? styles.table : styles.table_list}
-      >
-        {table.map((item) => {
-          let tiemedSlot = undefined;
-      
-          if (isListView) {
-            tiemedSlot = item.manager_appointments.find((appointment) => {
-              
-              return appointment.time === tableTime;
-            });
-          }
-          let timedSlotText;
-          if (isListView) {
-            timedSlotText = indefyTimedSlotText(
-              tiemedSlot.status ? tiemedSlot.status : tiemedSlot.status_id
-            );
-          }
+        {isLoading ? (
+          <div className={styles.loadingBackdrop}>
+            <TailSpin height="150px" width="150px" color="#999DFF" />
+          </div>
+        ) : null}
+        <ul
+          key={uuidv4()}
+          className={!isListView ? styles.table : styles.table_list}
+        >
+          {table.map((item) => {
+            let tiemedSlot = undefined;
 
-          return (
-            <React.Fragment key={uuidv4()}>
-              {isTableView && !isMarkUpAdded && (
-                <>
-                  {(isMarkUpAdded = true)}
-                  <TableMarkup key={uuidv4()} />
-                </>
-              )}
+            if (isListView) {
+              tiemedSlot = item.manager_appointments.find((appointment) => {
+                return appointment.time === tableTime;
+              });
+            }
 
-              {currentSelectedSortStatus &&
-              selectedManagerIds.includes(item.manager_id) === false ? (
-                <></>
-              ) : (
-                <ul key={uuidv4()} className={styles.managerUl}>
-                  <MeetingsTableItem
-                    key={uuidv4()}
-                    selectedManagerIds={selectedManagerIds}
-                    setSelectedManagerIds={setSelectedManagerIds}
-                    currentSelectedSortStatus={currentSelectedSortStatus}
-                    currentSelectedStars={currentSelectedStars}
-                    isManagerSelectedFr={selectedManagerIds.includes(
-                      item.manager_id
-                    )}
-                    text={item.manager_name}
-                    managerId={item.manager_id}
-                    managerName={true}
-                    date={date}
-                  />
+            let timedSlotText;
+            if (isListView) {
+              timedSlotText = indefyTimedSlotText(
+                tiemedSlot.status ? tiemedSlot.status : tiemedSlot.status_id
+              );
+            }
 
-                  {isTableLengthD ? (
-                    <MeetingsTableItem key={uuidv4()} text={"no data"} />
-                  ) : isListView && !isTableLengthD ? (
+            return (
+              <Fragment key={uuidv4()}>
+                {isTableView && !isMarkUpAdded && (
+                  <>
+                    {(isMarkUpAdded = true)}
+                    <TableMarkup key={uuidv4()} />
+                  </>
+                )}
+
+                {currentSelectedSortStatus &&
+                selectedManagerIds.includes(item.manager_id) === false ? (
+                  <></>
+                ) : (
+                  <ul key={uuidv4()} className={styles.managerUl}>
                     <MeetingsTableItem
                       key={uuidv4()}
-                      managerId={item.manager_id}
                       selectedManagerIds={selectedManagerIds}
                       setSelectedManagerIds={setSelectedManagerIds}
                       currentSelectedSortStatus={currentSelectedSortStatus}
                       currentSelectedStars={currentSelectedStars}
-                      text={timedSlotText}
                       isManagerSelectedFr={selectedManagerIds.includes(
                         item.manager_id
                       )}
-                      weekId={weekId}
-                      dayIndex={dayIndex}
+                      text={item.manager_name}
+                      managerId={item.manager_id}
+                      managerName={true}
                       date={date}
-                      hourIndex={tiemedSlot.time}
-                      colorId={tiemedSlot.status_id || tiemedSlot.status}
-                      slotId={tiemedSlot.slot_id}
-                      isFollowUp={tiemedSlot.follow_up}
-                      isOnControl={tiemedSlot.on_control}
                     />
-                  ) : (
-                    item.manager_appointments.map((i) => (
+
+                    {isTableLengthD ? (
+                      <MeetingsTableItem key={uuidv4()} text={"No data"} />
+                    ) : isListView && !isTableLengthD ? (
                       <MeetingsTableItem
                         key={uuidv4()}
                         managerId={item.manager_id}
                         selectedManagerIds={selectedManagerIds}
                         setSelectedManagerIds={setSelectedManagerIds}
+                        currentSelectedSortStatus={currentSelectedSortStatus}
+                        currentSelectedStars={currentSelectedStars}
+                        text={timedSlotText}
                         isManagerSelectedFr={selectedManagerIds.includes(
                           item.manager_id
                         )}
-                        currentSelectedSortStatus={currentSelectedSortStatus}
-                        currentSelectedStars={currentSelectedStars}
-                        text={i.text}
                         weekId={weekId}
-                        slotId={i.slot_id}
                         dayIndex={dayIndex}
                         date={date}
-                        hourIndex={i.time}
-                        colorId={i.status_id || i.status}
-                        isFollowUp={i.follow_up}
-                        isOnControl={i.on_control}
-                        isFreeze={i.is_freeze}
-                        onClickFn={() => {
-                          Fn(item.manager_id, weekId, dayIndex, i.time)
-                            .then(() => getNewTableData(parseInt(date.split('.')[0], 10), parseInt(date.split('.')[1], 10), parseInt(date.split('.')[2], 10)))
-                            .catch((error) => console.error("Error:", error));
-                        }}
+                        hourIndex={tiemedSlot.time}
+                        colorId={tiemedSlot.status_id || tiemedSlot.status}
+                        slotId={tiemedSlot.slot_id}
+                        isFollowUp={tiemedSlot.follow_up}
+                        isOnControl={tiemedSlot.on_control}
                       />
-                    ))
-                  )}
-                </ul>
-              )}
-            </React.Fragment>
-          );
-        })}
-      </ul>
+                    ) : (
+                      item.manager_appointments.map((i) => (
+                        <MeetingsTableItem
+                          key={uuidv4()}
+                          managerId={item.manager_id}
+                          selectedManagerIds={selectedManagerIds}
+                          setSelectedManagerIds={setSelectedManagerIds}
+                          isManagerSelectedFr={selectedManagerIds.includes(
+                            item.manager_id
+                          )}
+                          currentSelectedSortStatus={currentSelectedSortStatus}
+                          currentSelectedStars={currentSelectedStars}
+                          text={i.text}
+                          weekId={weekId}
+                          slotId={i.slot_id}
+                          dayIndex={dayIndex}
+                          date={date}
+                          hourIndex={i.time}
+                          colorId={i.status_id || i.status}
+                          isFollowUp={i.follow_up}
+                          isOnControl={i.on_control}
+                          isFreeze={i.is_freeze}
+                          onClickFn={() => {
+                            Fn(
+                              item.manager_id,
+                              weekId,
+                              dayIndex,
+                              i.time
+                            )
+                              .then(() =>
+                                getNewTableData(
+                                  parseInt(date.split(".")[0], 10),
+                                  parseInt(date.split(".")[1], 10),
+                                  parseInt(date.split(".")[2], 10)
+                                )
+                              )
+                              .catch((error) =>
+                                console.error("Error:", error)
+                              );
+                          }}
+                        />
+                      ))
+                    )}
+                  </ul>
+                )}
+              </Fragment>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
